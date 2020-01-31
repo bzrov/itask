739c6977ce69ded1874d1847191d5e839a36e5f1
bzrov <bzrov@mail.ru>
2020-01-31 12:05:42 +0300

    Updates

---
+++ precache-manifest.74d989fca06b992b9d94f922d6a01684.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d41c2b0bb5859f6395fdcccc797d1f6",
+    "url": "/itask/index.html"
+  },
+  {
+    "revision": "444155bad20c959d45ce",
+    "url": "/itask/static/css/main.0f077c51.chunk.css"
+  },
+  {
+    "revision": "8c2ff91fb6f399aa80bf",
+    "url": "/itask/static/js/2.cbce087f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/itask/static/js/2.cbce087f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "444155bad20c959d45ce",
+    "url": "/itask/static/js/main.3c9f01a5.chunk.js"
+  },
+  {
+    "revision": "9d4a1a0a575ded0d4682",
+    "url": "/itask/static/js/runtime-main.c0062158.js"
+  },
+  {
+    "revision": "c4adbb3c2297700a23444e77c558e1b9",
+    "url": "/itask/static/media/add-board-popup-bg.c4adbb3c.png"
+  },
+  {
+    "revision": "d90f7e6fe12d7646e42c6fe75fb1fe19",
+    "url": "/itask/static/media/board-error.d90f7e6f.gif"
+  }
+]);
